404d7ebacfbd196ef7a30c23b55f4858f4420dd9
Lado <lado.asambadze1@gmail.com>
2026-03-20 11:41:05 +0400

    f

--- frontend/lib/api/api.ts
+++ frontend/lib/api/api.ts
@@ -6,7 +6,7 @@ import {
 } from "../utils/auth";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/api",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
