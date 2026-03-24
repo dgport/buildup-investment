3b7e77899335c8c8f0300a22d201086c981ab68b
Lado <lado.asambadze1@gmail.com>
2026-03-24 17:25:42 +0400

    Fx

--- frontend/app/[locale]/contact/page.tsx
+++ frontend/app/[locale]/contact/page.tsx
@@ -1,10 +1,6 @@
 import { getPageMetadata } from "@/lib/getMetadata";
 import ContactSection from "./_components/ContactSection";
 
-export async function generateMetadata() {
-  return getPageMetadata("contact");
-}
-
 export default function Page() {
   return <ContactSection />;
 }
